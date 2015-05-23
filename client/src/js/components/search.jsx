d9aee17435f43af7d3597174ad871916b2212731
lifehackett <conor.d.hackett@gmail.com>
2015-05-23 08:00:44 -0400

    Added react-typeahead, basic setup with the componenet, filtering on artists and titles, and getters on MapStore for titles and artists

--- client/src/js/components/search.jsx
+++ client/src/js/components/search.jsx
@@ -1,17 +1,38 @@
 'use strict';
 
-var React = require('react');
+var React     = require('react');
+var Typeahead = require('react-typeahead').Typeahead;
+var MapStore  = require('../stores/mapStore');
 
 var Search = React.createClass({
+  getInitialState: function () {
+    return {
+      items: getSearchItems()
+    };
+  },
+
+  filterSearch: function (searchText, option) {
+    var result =  option.indexOf(searchText) > -1;
+
+    return result;
+  },
 
   render: function() {
     return (
-      <div>
-        This is the SEARCH
-      </div>
+      <Typeahead
+        options={ this.state.items }
+        maxVisible="5"
+        filterOption={ this.filterSearch } />
     );
   }
 
 });
 
 module.exports = Search;
+
+function getSearchItems () {
+  var titles = MapStore.getTitles();
+  var artists = MapStore.getArtists();
+
+  return titles.concat(artists);
+}
--- client/src/js/stores/mapStore.js
+++ client/src/js/stores/mapStore.js
@@ -1,15 +1,29 @@
 'use strict';
 
 var Reflux = require('reflux');
+var _      = require('lodash');
 
 var _heritageData = [
-  { title: 'title1', artist: 'artist1' },
-  { title: 'title2', artist: 'artist2' }
+  { title: 'Sistine Chapel', artist: 'Michelangelo' },
+  { title: 'David', artist: 'Michelangelo' },
+  { title: 'Water Lily Pond', artist: 'Monet' },
+  { title: 'The Magpie', artist: 'Monet' }
 ];
 
 var MapStore = Reflux.createStore({
   getHeritageItems: function () {
     return _heritageData;
+  },
+
+  getTitles: function () {
+    return _.pluck(_heritageData, 'title');
+  },
+
+  getArtists: function () {
+    return _.chain(_heritageData)
+      .pluck('artist')
+      .uniq()
+      .value();
   }
 });
 
