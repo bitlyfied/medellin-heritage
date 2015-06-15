24cf4551c6aa8e5b2510f2f19ff2467936e88bbd
lifehackett <conor.d.hackett@gmail.com>
2015-06-15 15:10:57 -0400

    Changed placeholder text

--- client/src/js/constants.js
+++ client/src/js/constants.js
@@ -5,7 +5,7 @@ var Constants = {
     'sculpture': 'images/icon-statue-25.png',
     'Escultura': 'images/icon-statue-25.png',
     'archSite': 'images/icon-dig-25.png',
-    'archSite in spanish': 'images/icon-dig-25.png'
+    'Hallazgo Arqueológico': 'images/icon-dig-25.png'
   }
 };
 
