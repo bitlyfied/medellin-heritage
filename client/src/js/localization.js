149ac16e883f58f79262893cd2d0d1797b4cb1cf
Mandy Moore <mandy@mobility-labs.com>
2015-06-15 10:44:45 -0500

    added spanish translations

---
+++ client/src/js/localization.js
@@ -0,0 +1,10 @@
+var localization = {
+  show: 'Mostrar',
+  learnMore: 'Más información',
+  directions: 'Cómo llegar',
+  title: 'Patrimonio de Medellín',
+  back: 'Back to Results',
+  newSearch: 'New Search'
+}
+
+module.exports = localization;
